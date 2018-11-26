34959a852ae6f300147d3d30bd040a4b77bc40d5
Jeff Hykin <jeffhykin@Jeffs-MBP-2.attlocal.net>
2018-11-26 08:12:29 -0600

    reduce size and fix small bug

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ Object.defineProperties(window.HTMLElement.prototype, {
         set: function(newChilden) {
             // remove all children
             while (this.firstChild) {
-                this.removeChild(myNode.firstChild);
+                this.removeChild(this.firstChild);
             }
             // add new child nodes
             for (let each of newChilden) {
@@ -60,139 +60,16 @@ window.HTMLElement.prototype[Symbol.iterator] = function* () {
     }
 }
 
-function SimpleElement(properties, ...children) {
-    return Object.assign(document.createElement(this.constructor.name.toLowerCase()), properties).add(...children)
-}
-
+// make it work with jsx
 let React = {
     createElement: (name, properties, ...children) => Object.assign(document.createElement(name), properties).add(...children)
 }
 
-let domElements = {
-    // constructors for all of the Dom
-    A          : function (...args) {return SimpleElement.apply(this, args)},
-    ABBR       : function (...args) {return SimpleElement.apply(this, args)},
-    ACRONYM    : function (...args) {return SimpleElement.apply(this, args)},
-    ADDRESS    : function (...args) {return SimpleElement.apply(this, args)},
-    APPLET     : function (...args) {return SimpleElement.apply(this, args)},
-    AREA       : function (...args) {return SimpleElement.apply(this, args)},
-    ARTICLE    : function (...args) {return SimpleElement.apply(this, args)},
-    ASIDE      : function (...args) {return SimpleElement.apply(this, args)},
-    AUDIO      : function (...args) {return SimpleElement.apply(this, args)},
-    B          : function (...args) {return SimpleElement.apply(this, args)},
-    BASE       : function (...args) {return SimpleElement.apply(this, args)},
-    BASEFONT   : function (...args) {return SimpleElement.apply(this, args)},
-    BDI        : function (...args) {return SimpleElement.apply(this, args)},
-    BDO        : function (...args) {return SimpleElement.apply(this, args)},
-    BIG        : function (...args) {return SimpleElement.apply(this, args)},
-    BLOCKQUOTE : function (...args) {return SimpleElement.apply(this, args)},
-    BODY       : function (...args) {return SimpleElement.apply(this, args)},
-    BR         : function (...args) {return SimpleElement.apply(this, args)},
-    BUTTON     : function (...args) {return SimpleElement.apply(this, args)},
-    CANVAS     : function (...args) {return SimpleElement.apply(this, args)},
-    CAPTION    : function (...args) {return SimpleElement.apply(this, args)},
-    CENTER     : function (...args) {return SimpleElement.apply(this, args)},
-    CITE       : function (...args) {return SimpleElement.apply(this, args)},
-    CODE       : function (...args) {return SimpleElement.apply(this, args)},
-    COL        : function (...args) {return SimpleElement.apply(this, args)},
-    COLGROUP   : function (...args) {return SimpleElement.apply(this, args)},
-    DATA       : function (...args) {return SimpleElement.apply(this, args)},
-    DATALIST   : function (...args) {return SimpleElement.apply(this, args)},
-    DD         : function (...args) {return SimpleElement.apply(this, args)},
-    DEL        : function (...args) {return SimpleElement.apply(this, args)},
-    DETAILS    : function (...args) {return SimpleElement.apply(this, args)},
-    DFN        : function (...args) {return SimpleElement.apply(this, args)},
-    DIALOG     : function (...args) {return SimpleElement.apply(this, args)},
-    DIR        : function (...args) {return SimpleElement.apply(this, args)},
-    DIV        : function (...args) {return SimpleElement.apply(this, args)},
-    DL         : function (...args) {return SimpleElement.apply(this, args)},
-    DT         : function (...args) {return SimpleElement.apply(this, args)},
-    EM         : function (...args) {return SimpleElement.apply(this, args)},
-    EMBED      : function (...args) {return SimpleElement.apply(this, args)},
-    FIELDSET   : function (...args) {return SimpleElement.apply(this, args)},
-    FIGCAPTION : function (...args) {return SimpleElement.apply(this, args)},
-    FIGURE     : function (...args) {return SimpleElement.apply(this, args)},
-    FONT       : function (...args) {return SimpleElement.apply(this, args)},
-    FOOTER     : function (...args) {return SimpleElement.apply(this, args)},
-    FORM       : function (...args) {return SimpleElement.apply(this, args)},
-    FRAME      : function (...args) {return SimpleElement.apply(this, args)},
-    FRAMESET   : function (...args) {return SimpleElement.apply(this, args)},
-    H1         : function (...args) {return SimpleElement.apply(this, args)},
-    H2         : function (...args) {return SimpleElement.apply(this, args)},
-    H3         : function (...args) {return SimpleElement.apply(this, args)},
-    H4         : function (...args) {return SimpleElement.apply(this, args)},
-    H5         : function (...args) {return SimpleElement.apply(this, args)},
-    H6         : function (...args) {return SimpleElement.apply(this, args)},
-    HEAD       : function (...args) {return SimpleElement.apply(this, args)},
-    HEADER     : function (...args) {return SimpleElement.apply(this, args)},
-    HR         : function (...args) {return SimpleElement.apply(this, args)},
-    HTML       : function (...args) {return SimpleElement.apply(this, args)},
-    I          : function (...args) {return SimpleElement.apply(this, args)},
-    IFRAME     : function (...args) {return SimpleElement.apply(this, args)},
-    IMG        : function (...args) {return SimpleElement.apply(this, args)},
-    INPUT      : function (...args) {return SimpleElement.apply(this, args)},
-    INS        : function (...args) {return SimpleElement.apply(this, args)},
-    KBD        : function (...args) {return SimpleElement.apply(this, args)},
-    LABEL      : function (...args) {return SimpleElement.apply(this, args)},
-    LEGEND     : function (...args) {return SimpleElement.apply(this, args)},
-    LI         : function (...args) {return SimpleElement.apply(this, args)},
-    LINK       : function (...args) {return SimpleElement.apply(this, args)},
-    MAIN       : function (...args) {return SimpleElement.apply(this, args)},
-    MAP        : function (...args) {return SimpleElement.apply(this, args)},
-    MARK       : function (...args) {return SimpleElement.apply(this, args)},
-    META       : function (...args) {return SimpleElement.apply(this, args)},
-    METER      : function (...args) {return SimpleElement.apply(this, args)},
-    NAV        : function (...args) {return SimpleElement.apply(this, args)},
-    NOFRAMES   : function (...args) {return SimpleElement.apply(this, args)},
-    NOSCRIPT   : function (...args) {return SimpleElement.apply(this, args)},
-    OBJECT     : function (...args) {return SimpleElement.apply(this, args)},
-    OL         : function (...args) {return SimpleElement.apply(this, args)},
-    OPTGROUP   : function (...args) {return SimpleElement.apply(this, args)},
-    OPTION     : function (...args) {return SimpleElement.apply(this, args)},
-    OUTPUT     : function (...args) {return SimpleElement.apply(this, args)},
-    P          : function (...args) {return SimpleElement.apply(this, args)},
-    PARAM      : function (...args) {return SimpleElement.apply(this, args)},
-    PICTURE    : function (...args) {return SimpleElement.apply(this, args)},
-    PRE        : function (...args) {return SimpleElement.apply(this, args)},
-    PROGRESS   : function (...args) {return SimpleElement.apply(this, args)},
-    Q          : function (...args) {return SimpleElement.apply(this, args)},
-    RP         : function (...args) {return SimpleElement.apply(this, args)},
-    RT         : function (...args) {return SimpleElement.apply(this, args)},
-    RUBY       : function (...args) {return SimpleElement.apply(this, args)},
-    S          : function (...args) {return SimpleElement.apply(this, args)},
-    SAMP       : function (...args) {return SimpleElement.apply(this, args)},
-    SCRIPT     : function (...args) {return SimpleElement.apply(this, args)},
-    SECTION    : function (...args) {return SimpleElement.apply(this, args)},
-    SELECT     : function (...args) {return SimpleElement.apply(this, args)},
-    SMALL      : function (...args) {return SimpleElement.apply(this, args)},
-    SOURCE     : function (...args) {return SimpleElement.apply(this, args)},
-    SPAN       : function (...args) {return SimpleElement.apply(this, args)},
-    STRIKE     : function (...args) {return SimpleElement.apply(this, args)},
-    STRONG     : function (...args) {return SimpleElement.apply(this, args)},
-    STYLE      : function (...args) {return SimpleElement.apply(this, args)},
-    SUB        : function (...args) {return SimpleElement.apply(this, args)},
-    SUMMARY    : function (...args) {return SimpleElement.apply(this, args)},
-    SUP        : function (...args) {return SimpleElement.apply(this, args)},
-    SVG        : function (...args) {return SimpleElement.apply(this, args)},
-    TABLE      : function (...args) {return SimpleElement.apply(this, args)},
-    TBODY      : function (...args) {return SimpleElement.apply(this, args)},
-    TD         : function (...args) {return SimpleElement.apply(this, args)},
-    TEMPLATE   : function (...args) {return SimpleElement.apply(this, args)},
-    TEXTAREA   : function (...args) {return SimpleElement.apply(this, args)},
-    TFOOT      : function (...args) {return SimpleElement.apply(this, args)},
-    TH         : function (...args) {return SimpleElement.apply(this, args)},
-    THEAD      : function (...args) {return SimpleElement.apply(this, args)},
-    TIME       : function (...args) {return SimpleElement.apply(this, args)},
-    TITLE      : function (...args) {return SimpleElement.apply(this, args)},
-    TR         : function (...args) {return SimpleElement.apply(this, args)},
-    TRACK      : function (...args) {return SimpleElement.apply(this, args)},
-    TT         : function (...args) {return SimpleElement.apply(this, args)},
-    U          : function (...args) {return SimpleElement.apply(this, args)},
-    UL         : function (...args) {return SimpleElement.apply(this, args)},
-    VAR        : function (...args) {return SimpleElement.apply(this, args)},
-    VIDEO      : function (...args) {return SimpleElement.apply(this, args)},
-    WBR        : function (...args) {return SimpleElement.apply(this, args)},
-    makeCustom : (constructor, extend) => window.customElements.define(constructor.name.toLowerCase()+"-", constructor),
+// add all the dom elements
+let domElements = {}
+tagNames = ["a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr"]
+for (let each of tagNames) {
+    eval(`domElements.${each.toUpperCase()} = (properties,...children) => Object.assign(document.createElement(${each}), properties).add(...children)`)
 }
 
 function makeGlobal() {
@@ -202,11 +79,11 @@ function makeGlobal() {
 
 // if there is no exporting system
 if(typeof exports == "undefined"){
-    // put everything in the global scope
+    // put everything in the window scope
     makeGlobal()
 // if there is an export system
 } else {
-    // give the user the choice of local or global
+    // give the user the choice of local or window
     module.exports = domElements
     module.exports.global = makeGlobal
 }
