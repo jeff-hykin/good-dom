c8b78de077af46e5e9808bbe3490610ea91dfeba
Jeff Hykin <jeff.hykin@gmail.com>
2021-09-07 08:38:40 -0500

    add support for SVG elements

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // expand the HTML element ability
 Object.defineProperties(window.HTMLElement.prototype, {
     // setting styles through a string
-    css : { set: Object.getOwnPropertyDescriptor(window.HTMLElement.prototype, 'style').set },
+    css: { set: Object.getOwnPropertyDescriptor(window.HTMLElement.prototype, 'style').set },
     // allow setting of styles through string or object
     style: {
         set: function (styles) {
@@ -29,16 +29,16 @@ Object.defineProperties(window.HTMLElement.prototype, {
         }
     },
     class: {
-        set : function(newClass) {
+        set: function(newClass) {
             this.className = newClass
         },
-        get : function() {
+        get: function() {
             return this.className
         }
     }
 })
 // add()
-window.HTMLElement.prototype.add = function (...inputs) {
+window.HTMLElement.prototype.add = window.SVGElement.prototype.add = window.HTMLSelectElement.prototype.add = function (...inputs) {
     for (let each of inputs) {
         if (typeof each == 'string') {
             this.appendChild(new window.Text(each))
@@ -52,12 +52,12 @@ window.HTMLElement.prototype.add = function (...inputs) {
     }
     return this
 }
-// the special "add" case of the select method
-window.HTMLSelectElement.prototype.add = window.HTMLElement.prototype.add
+
 // addClass()
 window.HTMLElement.prototype.addClass = function (...inputs) {
     return this.classList.add(...inputs)
 }
+
 // for (let eachChild of elemCollection)
 window.HTMLCollection.prototype[Symbol.iterator] = function* () {
     let index = 0
@@ -75,7 +75,7 @@ window.HTMLElement.prototype[Symbol.iterator] = function* () {
     }
 }
 // create a setter/getter for <head>
-let originalHead = document.head
+const originalHead = document.head
 // add a setter to document.head
 Object.defineProperty(document,"head", { 
     set: (element) => {
@@ -84,19 +84,60 @@ Object.defineProperty(document,"head", {
     get: ()=>originalHead
 })
 
+// 
 // add all the dom elements
-let domElements = {}
-tagNames = ["a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr"]
-for (let each of tagNames) {
-    eval(`domElements.${each.toUpperCase()} = function(properties,...children) { 
-        if (properties instanceof window.Node || typeof properties == 'string') 
-            return document.createElement("${each}").add(properties, ...children)
-        return Object.assign(document.createElement("${each}"), properties).add(...children)}
-        `)
+// 
+const elements = {}
+const tagNames = ["a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr"]
+for (const each of tagNames) {
+    elements[each.toUpperCase()] = function(properties, ...children) {
+        // if only given children
+        if (properties instanceof window.Node || typeof properties == 'string') {
+            children.unshift(properties)
+            properties = null
+        }
+        // create either an html element or an svg element
+        const element = document.createElement(each)
+        if (properties instanceof Object) {
+            for (const [key, value] of Object.entries(properties)) {
+                try {
+                    element.setAttribute(key, value)
+                } catch (error) {
+                    element[key] = value
+                }
+            }
+        }
+        return element.add(...children)
+    }
+}
+// 
+// add all the (exclusively) svg elements (because <a> tags are both SVG and Dom *facepalm*)
+// 
+const exclusivelySvgElements = ["animate", "animateMotion", "animateTransform", "circle", "clipPath", "defs", "desc", "discard", "ellipse", "feBlend", "feColorMatrix", "feComponentTransfer", "feComposite", "feConvolveMatrix", "feDiffuseLighting", "feDisplacementMap", "feDistantLight", "feDropShadow", "feFlood", "feFuncA", "feFuncB", "feFuncG", "feFuncR", "feGaussianBlur", "feImage", "feMerge", "feMergeNode", "feMorphology", "feOffset", "fePointLight", "feSpecularLighting", "feSpotLight", "feTile", "feTurbulence", "filter", "foreignObject", "g", "hatch", "hatchpath", "image", "line", "linearGradient", "marker", "mask", "mesh", "meshgradient", "meshpatch", "meshrow", "metadata", "mpath", "path", "pattern", "polygon", "polyline", "radialGradient", "rect", "set", "stop", "switch", "symbol", "text", "textPath", "tspan", "unknown", "use", "view",]
+for (const each of tagNames) {
+    elements[each.toUpperCase()] = function(properties, ...children) {
+        // if only given children
+        if (properties instanceof window.Node || typeof properties == 'string') {
+            children.unshift(properties)
+            properties = null
+        }
+        // create either an html element or an svg element
+        const element = document.createElementNS('http://www.w3.org/2000/svg', each)
+        if (properties instanceof Object) {
+            for (const [key, value] of Object.entries(properties)) {
+                try {
+                    element.setAttribute(key, value)
+                } catch (error) {
+                    element[key] = value
+                }
+            }
+        }
+        return element.add(...children)
+    }
 }
 
 function makeGlobal() {
-    Object.assign(window, domElements)
+    Object.assign(window, elements)
 }
 
 // if there is no exporting system
@@ -106,6 +147,6 @@ if(typeof exports == "undefined"){
 // if there is an export system
 } else {
     // give the user the choice of local or window
-    module.exports = domElements
+    module.exports = elements
     module.exports.global = makeGlobal
 }
